40b444dee1bfda8e723372b9d8d16aba67093eea
Kelly-coder <kelechiawa11@gmail.com>
2026-02-24 17:59:23 +0100

    four commit

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,19 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '.prisma/client';
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+let prisma: PrismaClient;
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma;
-
+export { prisma };
 export default prisma;
